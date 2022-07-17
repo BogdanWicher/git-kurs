f353ff1bc5febe817058b1d36a8f61cc40b837ad
bogdan <albomi14@gmail.com>
2022-07-17 17:07:57 +0200

    pliki js

---
+++ js/1.js
@@ -0,0 +1,14 @@
+const suma=(a,b)=>
+{
+    return a+b;
+}
+
+const roznica=(a,b)=>
+{
+    return a-b;
+}
+console.log(suma(5,6))
+console.log(roznica(5,6))
+console.log("Koniec")
+
+module.exports={roznica,suma};
---
+++ js/2.js
@@ -0,0 +1,5 @@
+const mata=require("./1");
+
+console.log(mata.suma(5,6))
+console.log(mata.roznica(5,6))
+console.log("koniec 2")
